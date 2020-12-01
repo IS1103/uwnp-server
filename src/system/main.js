5956ba487ea76944c643a13ab59be212584e5822
is <is100602@gmail.com>
2020-12-01 11:48:59 +0800

    add log

--- src/system/main.js
+++ src/system/main.js
@@ -1,26 +1,4 @@
-
-const log4js = require('log4js');
-log4js.configure({
-  appenders: { serverLog: { type: "file", filename: "logs/server.log" }, console: { type: 'console' } },
-  categories: { default: { appenders: ['serverLog', 'console'], level: "all" } }
-});
-console.log(222);
-console.debug(222);
-console.error(666);
-const logger = log4js.getLogger();
-logger.trace("Entering cheese testing");
-logger.debug("Got cheese.");
-logger.info("Cheese is Comté.");
-logger.warn("Cheese is quite smelly.");
-logger.error("Cheese is too ripe!");
-logger.fatal("Cheese was breeding ground for listeria.");
-
-console.info = function () {
-  // logger.info.call(null, arguments);
-  logger.info(arguments);
-}
-console.info('null', 9, { s: 5 });
-console.log("s", { '5': 99 });
+const log = require('./log');
 
 class Main {
   config = {};
@@ -30,11 +8,6 @@ class Main {
   wss = null;
 
   constructor() {
-    this.setupLog();
-  }
-
-  setupLog() {
-
   }
 
   use(middleware) {
